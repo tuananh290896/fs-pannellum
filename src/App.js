1940ec66179ea080c29bb60af466488c2288134a
anh.tran <anh.tran@tomosia.com>
2020-08-25 21:16:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import ReactDOM from "react-dom";
+import { Pannellum } from "pannellum-react";
+import Logo from "./images/logo.png";
+import data from './data/data.json'
+
+import "./index.css";
+
+function parseHTML(html) {
+  var t = document.createElement('template');
+  t.innerHTML = html;
+  return t.content.cloneNode(true);
+}
 
 function App() {
+  const [item, setImage] = useState(data)
+
+  const renderHotspot = (hotSpotDiv, args) =>{
+    hotSpotDiv.classList.add('custom-tooltip');
+    var span = document.createElement('span');
+    var toolTip = parseHTML(`<div class="pannellumn-tootip"><p>${args.title}</p></div>`)
+    hotSpotDiv.appendChild(span);
+    hotSpotDiv.appendChild(toolTip);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Pannellum
+        width="100%"
+        height="100vh"
+        image={item.image}
+        pitch={item.pitch}
+        yaw={item.yaw}
+        hfov={200}
+        autoLoad
+        showZoomCtrl={false}
+        onLoad={() => {
+          console.log("panorama loaded");
+        }}
+      >
+        {
+          item.hotSpots && item.hotSpots.map((hotspot, index) =>{
+            return <Pannellum.Hotspot
+            type="custom"
+            key={index}
+            pitch={hotspot.pitch}
+            yaw={hotspot.yaw}
+            tooltip={renderHotspot}
+            tooltipArg={{'title': hotspot.title }}
+            handleClick={(evt , args) => setImage(args.item)}
+            handleClickArg={{ "item": hotspot }}
+          />
+          })
+        }
+      </Pannellum>
+
+      <div className="header">
+          <img src={Logo}  alt="logo Fservice" className="header-brand__logo"/>
+      </div>
     </div>
   );
 }
