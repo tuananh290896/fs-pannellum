43f55c6cea4cede8a98928879931f879e266b8e9
anh.tran <anh.tran@tomosia.com>
2020-08-25 21:25:53 +0700

    Updates

---
+++ precache-manifest.b4b65f83ae266fdacdd75c3972f5d990.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16b382bb804efd47eec01ecb1a895c1",
+    "url": "/fs-pannellum/index.html"
+  },
+  {
+    "revision": "f53ea1f2ecd4868d9f1f",
+    "url": "/fs-pannellum/static/css/2.428b0737.chunk.css"
+  },
+  {
+    "revision": "8990961a8a7e42aca170",
+    "url": "/fs-pannellum/static/css/main.22cbaec3.chunk.css"
+  },
+  {
+    "revision": "f53ea1f2ecd4868d9f1f",
+    "url": "/fs-pannellum/static/js/2.4c0f180f.chunk.js"
+  },
+  {
+    "revision": "4eba80e21df2e21401f79434de624c20",
+    "url": "/fs-pannellum/static/js/2.4c0f180f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8990961a8a7e42aca170",
+    "url": "/fs-pannellum/static/js/main.b9da5531.chunk.js"
+  },
+  {
+    "revision": "046b95db4e1e1dca3681",
+    "url": "/fs-pannellum/static/js/runtime-main.a3ae1d29.js"
+  },
+  {
+    "revision": "b5fe02a429074b0c8ffa3fa98bbef62e",
+    "url": "/fs-pannellum/static/media/3d_rotation-24px.b5fe02a4.svg"
+  },
+  {
+    "revision": "9f2131c2974ff6dcfefe82f5727c466d",
+    "url": "/fs-pannellum/static/media/background.9f2131c2.svg"
+  },
+  {
+    "revision": "7bd9a61f8f1ba09b4d7f957d243470d7",
+    "url": "/fs-pannellum/static/media/compass.7bd9a61f.svg"
+  },
+  {
+    "revision": "adb137f0fa13eb025c13d13c727fe7fa",
+    "url": "/fs-pannellum/static/media/grab.adb137f0.svg"
+  },
+  {
+    "revision": "3acfddd5c09c90a4f089f8d626aebca1",
+    "url": "/fs-pannellum/static/media/grabbing.3acfddd5.svg"
+  },
+  {
+    "revision": "72a889053d04914e041abbbfbd2df534",
+    "url": "/fs-pannellum/static/media/icon-walking.72a88905.svg"
+  },
+  {
+    "revision": "9164dab8d0798f57f2aae0f1db7c1c16",
+    "url": "/fs-pannellum/static/media/logo.9164dab8.png"
+  },
+  {
+    "revision": "f37aa8ca72b5548a255c62dab1614437",
+    "url": "/fs-pannellum/static/media/sprites.f37aa8ca.svg"
+  }
+]);
